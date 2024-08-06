d93fd7ac3e139dad2378546e8d4cd71678ecb45f
Mahmoud Qaddura <kmahmoud1898@gmail.com>
2024-08-06 03:06:52 -0700

    asdas

--- front/src/components/navbar/Logo.jsx
+++ front/src/components/navbar/Logo.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 
 const Logo = () => {
-    return (
-        <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="/images/logo.svg" className="h-9 w-9" alt="OpenMedia Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">OpenMedia</span>
-        </Link>
-    )
-}
+  return (
+    <Link
+      to="/home"
+      className="flex items-center space-x-3 rtl:space-x-reverse"
+    >
+      <img src="/images/logo.svg" className="h-9 w-9" alt="OpenMedia Logo" />
+      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+        OpenMedia
+      </span>
+    </Link>
+  );
+};
 
 export default Logo;
